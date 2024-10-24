9687818319895448c7accf3d58d0bae1b797049a
Arthur0Martinez <martinez.arthur2602@gmail.com>
2024-10-24 22:43:45 +0200

    Ajout de CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const passwordRoutes = require('./routes/passwords');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/passwords', passwordRoutes);
 
